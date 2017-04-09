385fb395a72214757886be41f1fe5d64643cd0ad
Samuel Haugland <samhaug@umich.edu>
2017-04-09 18:41:58 -0400

    align_on_conversion

--- align_on_conversion.py
+++ align_on_conversion.py
@@ -6,7 +6,7 @@
 File Name : align_on_conversion.py
 Purpose : align traces on S1800P by drag-and-drop
 Creation Date : 07-04-2017
-Last Modified : Sat 08 Apr 2017 05:39:15 PM EDT
+Last Modified : Sun 09 Apr 2017 05:58:42 PM EDT
 Created By : Samuel M. Haugland
 
 ==============================================================================
@@ -26,28 +26,62 @@ model = TauPyModel(model='prem50')
 
 def main():
     st = read_stream()
+    data_dict,gcarc_list = window_phase(st)
     fig,ax = plt.subplots(figsize=(10,15))
-    for idx,tr in enumerate(st):
-        e = tr.stats.npts/tr.stats.sampling_rate
-        l = tr.stats.station
+    for idx,keys in enumerate(data_dict):
+        #e = tr.stats.npts/tr.stats.sampling_rate
+        #l = tr.stats.station
         #t = np.linspace(0,850,num=tr.stats.npts)
-        gcarc = tr.stats.sac['gcarc']
-        ax.plot(gcarc+tr.data,color='k',label=l,pickradius=50,picker=True)
+        ax.plot(gcarc_list[idx]+data_dict[keys],color='k',label=keys,pickradius=50,picker=True)
 
+    ax.axvline(len(data_dict[keys])/2.,color='b')
+    ax.axvline(len(data_dict[keys])*(1./6),color='b')
+    ax.axvline(len(data_dict[keys])*(2./6),color='b')
+    ax.axvline(len(data_dict[keys])*(4./6),color='b')
+    ax.axvline(len(data_dict[keys])*(5./6),color='b')
     dragh = DragHandler(figure=fig)
     plt.show()
-    print zip(dragh.station,dragh.shift)
+    roll_list = zip(dragh.station,dragh.shift)
+    #print roll_list
+    data_dict = roll_data(roll_list,data_dict)
+    write_h5(data_dict)
+
+def write_h5(data_dict):
+    array = np.array([data_dict[keys] for keys in data_dict])
+    f = h5py.File('align_conversion.h5','w')
+    f.create_dataset('wave',data=array)
+    f.close()
+
+def roll_data(roll_list,data_dict):
+    for ii in roll_list:
+        data_dict[ii[0]] = np.roll(data_dict[ii[0]],ii[1])
+    return data_dict
+
+def window_phase(st):
+    data_dict = {}
+    gcarc_list = []
+    for idx,tr in enumerate(st):
+        d = seispy.data.phase_window(st[idx],phase=['S1800P'],window=(-30,30))
+        data_dict[tr.stats.station] = d.data
+        gcarc_list.append(tr.stats.sac['gcarc'])
+    return data_dict,gcarc_list
 
 def read_stream():
-    st = obspy.read('/home/samhaug/work1/SP_brazil_data/2007-07-21-mw60-western-brazil-4/sparse_Z.pk')
-    st.normalize()
-    st = seispy.data.align_on_phase(st,phase=['P'])
-    for idx,tr in enumerate(st):
-        st[idx] = seispy.data.phase_window(st[idx],['P'],window=(-50,800))
+    stz = obspy.read('/home/samhaug/work1/SP_brazil_data/2007-07-21-mw60-western-brazil-4/sparse_Z.pk')
+    str = obspy.read('/home/samhaug/work1/SP_brazil_data/2007-07-21-mw60-western-brazil-4/sparse_R.pk')
+    stz = seispy.data.align_on_phase(stz,phase=['P'])
+    str.filter('highpass',freq=1./15)
+    stz.filter('highpass',freq=1./15)
+    for idx,tr in enumerate(str):
+        d = seispy.data.phase_window(str[idx],['S'],window=(-30,30))
+        e = np.abs(d.data).max()
+        #st[idx] = seispy.data.phase_window(st[idx],['P'],window=(-50,800))
+        stz[idx].data *= 1./e
         tr.stats.location = tr.stats.sac['gcarc']
-    st.interpolate(50)
-    st.sort(['location'])
-    return st
+    stz.interpolate(50)
+    stz.sort(['location'])
+    stz = seispy.data.align_on_phase(stz)
+    return stz
 
 class DragHandler(object):
     """ A simple class to handle Drag n Drop.
@@ -55,9 +89,6 @@ class DragHandler(object):
     This is a simple example, which works for Text objects only.
     """
     def __init__(self, figure=None) :
-        """ Create a new drag handler and connect it to the figure's event system.
-        If the figure handler is not given, the current figure is used instead
-        """
         if figure is None : figure = p.gcf()
         # simple attibute to store the dragged text object
         self.dragged = None
@@ -69,7 +100,6 @@ class DragHandler(object):
         figure.canvas.mpl_connect("button_release_event", self.on_release_event)
 
     def on_pick_event(self, event):
-        " Store which text object was picked and were the pick event occurs."
 
         self.dragged = event.artist
         self.xdata = event.artist.get_data()[0]
@@ -79,9 +109,7 @@ class DragHandler(object):
         return True
 
     def on_release_event(self, event):
-        " Update text position and redraw"
 
-        #if self.dragged is not None :
         newx = event.xdata
         newy = np.roll(self.ydata,int(newx-self.pick_pos))
         self.dragged.set_data(self.xdata,newy)
@@ -89,7 +117,6 @@ class DragHandler(object):
         p.draw()
         self.shift.append(int(newx-self.pick_pos))
         return True
-
 
 main()
 
---
+++ bootstrap_conversion.py
@@ -0,0 +1,46 @@
+#!/home/samhaug/anaconda2/bin/python
+
+'''
+==============================================================================
+
+File Name : bootstrap_conversion.py
+Purpose : Use to bootstrap the aligned h5py phases. Need to use align_on_conversion.py
+Creation Date : 09-04-2017
+Last Modified : Sun 09 Apr 2017 03:27:47 PM EDT
+Created By : Samuel M. Haugland
+
+==============================================================================
+'''
+
+import numpy as np
+from matplotlib import pyplot as plt
+from subprocess import call
+from os import listdir
+import h5py
+import obspy
+import seispy
+
+def main():
+    conv = read_h5()
+    bootstrap(conv,100)
+
+def read_h5():
+    f = h5py.File('align_conversion.h5','r')
+    conv = f['wave'][...]
+    return conv
+
+def bootstrap(conv,iterations):
+    print 'bootstrap'
+    boot_list = []
+    for ii in range(iterations):
+        single = []
+        for jj in range(0,conv.shape[0]):
+            idx = np.random.randint(0,conv.shape[0])
+            single.append(conv[idx])
+        single = np.array(single).mean(axis=0)
+        boot_list.append(single)
+    f = h5py.File('align_bootstrap.h5','w')
+    f.create_dataset('boot',data = np.array(boot_list))
+    f.close()
+
+main()
